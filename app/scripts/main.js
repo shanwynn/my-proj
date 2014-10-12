340bc19b962177201056ef8de709162f5e0da83f
Shannon Wynn <shannonwynn@mac.com>
2014-10-12 14:12:29 -0400

    more progress, need to do the js and tab menus

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,27 +1,13 @@
 /*AJAX Pull data from GitHub API (2 links on Basecamp) pull data, assign to variables.
 profile and repo information, create templates (<%    %>) in HTML where I'll put data.*/
 
-/*var profileInfo = function () {
+var profile = function () {
   $.getJSON('https://api.github.com/users/shanwynn', function (data) {
     $('.info').html(JSON.stringify(data));
     setProfileInfo(data);
   });
 };
 
-var repoInfo = function () {
-  $.getJSON('https://api.github.com/users/shanwynn/repos', function (data) {
-    $('.info').html(JSON.stringify(data));
-    setrepoInfo(data);
-  });
-};*/
-
-var profileTemplate = _.template($('data').html);
-function getProfile (profileName) {
-  $.getJSON('https://api.github.com/users/shanwynn', function (data) {
-    $('.info').html(JSON.stringify(data));
-
-var compiledTemplate=template(data);
-      $('#results').append(compiledTemplate);
-
-  });
-}
+var profileTemplateString = $('#profileTemplate').html();
+var profileTemplate = _.template(personTemplateString);
+var finishedProfileTemplate=profileTemplate(profile);
