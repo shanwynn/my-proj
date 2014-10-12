e1e928ba1c66189bff60697db96ff1dfae596440
Shannon Wynn <shannonwynn@mac.com>
2014-10-11 23:05:17 -0400

    More html css nothing js file non-functional

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,7 @@
 /*AJAX Pull data from GitHub API (2 links on Basecamp) pull data, assign to variables.
 profile and repo information, create templates (<%    %>) in HTML where I'll put data.*/
 
-var profileInfo = function () {
+/*var profileInfo = function () {
   $.getJSON('https://api.github.com/users/shanwynn', function (data) {
     $('.info').html(JSON.stringify(data));
     setProfileInfo(data);
@@ -13,4 +13,15 @@ var repoInfo = function () {
     $('.info').html(JSON.stringify(data));
     setrepoInfo(data);
   });
-};
+};*/
+
+var profileTemplate = _.template($('data').html);
+function getProfile (profileName) {
+  $.getJSON('https://api.github.com/users/shanwynn', function (data) {
+    $('.info').html(JSON.stringify(data));
+
+var compiledTemplate=template(data);
+      $('#results').append(compiledTemplate);
+
+  });
+}
